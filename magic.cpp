c03f83adc4b64aba78cb84e298018f4adce6f627
loganfalzarano <lfalzar1@jhu.edu>
2022-04-07 13:16:59 -0400

    need to deal with symbols

--- magic.cpp
+++ magic.cpp
@@ -48,14 +48,37 @@ int main(int argc, char **argv) {
   //cout << ".shstrtab section index is %u\n"
 
   printf(".shstrtab section index is %u\n", elf_header->e_shstrndx);
-  return 0;
 
-  Elf64_Shdr *section_headers = (Elf64_Shdr *) (elf_header + elf_header->e_shoff);
+  //gets the index of the headers that contains string table
+  uint32_t string_table_section_index = elf_header->e_shstrndx;
 
+  //gets the total number of section headers
+  uint32_t num_section_headers = elf_header->e_shnum;
+
+  //gets pointer to the sections headers
+  unsigned char *section_headers_pointer = (unsigned char *)(elf_header) + elf_header->e_shoff;
+
+  //casts pointer to Elf64_Shdr so we can access the fields
+  Elf64_Shdr *section_headers = (Elf64_Shdr *) section_headers_pointer;
+
+  //accesses section with string table
   Elf64_Shdr string_table_section = section_headers[string_table_section_index];
 
-  cout << section_headers[1].sh_name << endl;
-  uint32_t name_index = section_headers[1].sh_name;
-  cout << string_table_section[name_index];
+  //gets a pointer to the beginning of the string table data
+  const char *shstrtab_data = ((char *) data) + string_table_section.sh_offset;
+
+  //gets pointer to first section header
+  Elf64_Shdr * sec_header = &(section_headers[0]);
+
+  //gets the name of the first section header
+  //const char *section_name = shstrtab_data + sec_header->sh_name;
+
+  
+  
+  for (uint32_t i=0; i<num_section_headers; i++) {
+    Elf64_Shdr curr = section_headers[i];
+    const char *section_name = shstrtab_data + curr.sh_name;
+    printf("Section header %d: name=%s, type=%lx, offset=%lx, size=%lx\n", i, section_name, curr.sh_type, curr.sh_offset, curr.sh_size);
+  }
 
 }
